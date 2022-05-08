95eaf4d14ed825dc390f6bc40d1ec63702bc52c1
Carlos González <carlos.rgn@gmail.com>
2022-05-07 20:57:47 -0600

    scrape

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.Playwright;
+
 Console.WriteLine("Hello, World!");
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = true });
+var page = await browser.NewPageAsync();
+await page.GotoAsync("https://wikipedia.org");
+var slogan = await page.QuerySelectorAsync(".localized-slogan");
+Console.WriteLine("slogan: {0}", await slogan!.TextContentAsync());
